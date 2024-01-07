4a95e091d70472320731f8dbdf101bf404e6f23b
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-01-08 01:37:32 +0200

    implement routers

--- server.js
+++ server.js
@@ -4,9 +4,36 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Heeeellooo');
+const items = [
+    {
+        id: 0,
+        name: 'apple'
+    },
+    {
+        id: 1,
+        name: 'orange'
+    }];
+
+
+
+app.get('/items', (req, res) => {
+    res.json(items);  
 })
+
+
+
+app.get('/items/:itemId', (req, res) => {
+    const itemId = Number(req.params.itemId);
+    const item = items[itemId];
+    if (item) {
+        res.status(200).json(item);
+    }else {
+        res.status(404).json({
+            error:"item doesn't exist"
+        });
+    }
+})
+
 
 app.get('/messages', (req, res) => {
     res.send('<ul><li>Hello is it me!</li></ul>');
@@ -16,6 +43,7 @@ app.post('/messages', (req, res) => {
     console.log('Updating my ass..');
 });
 
+// three different routers. each pairing an http method with the name of a route.
 
 
 
